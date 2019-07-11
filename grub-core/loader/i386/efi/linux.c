5c51903f4f64b30527a2c4cca9db56efba41ebf9
Peter Jones <pjones@redhat.com>
2019-07-11 17:17:02 +0200

    Try to pick better locations for kernel and initrd
    - Don't limit allocations on 64-bit platforms to < 0x[37f]fffffff if
      we're using the "large" code model ; use __UINTPTR_MAX__.
    - Get the comparison right to check the address we've allocated.
    - Fix the allocation for the command line as well.
    
    *But*, when we did this some systems started failing badly; coudln't
    parse partition tables, etc.  What's going on here is the disk controller
    is silently failing DMAs to addresses above 4GB, so we're trying to parse
    uninitialized (or HW zeroed) ram when looking for the partition table,
    etc.
    
    So to limit this, we make grub_malloc() pick addresses below 4GB on
    x86_64, but the direct EFI page allocation functions can get addresses
    above that.
    
    Additionally, we now try to locate kernel+initrd+cmdline+etc below
    0x7fffffff, and if they're too big to fit any memory window there, then
    we try a higher address.
    
    (backport from rhboot 9e6c1d803ade111b8719502ff25e86d8b4564de8)
    Signed-off-by: Peter Jones <pjones@redhat.com>
    [david.abdurachmanov: fix macro for riscv64]
    Signed-off-by: David Abdurachmanov <david.abdurachmanov@sifive.com>
    Signed-off-by: Robbie Harwood <rharwood@redhat.com>

--- grub-core/loader/i386/efi/linux.c
+++ grub-core/loader/i386/efi/linux.c
@@ -28,6 +28,7 @@
 #include <grub/efi/efi.h>
 #include <grub/efi/linux.h>
 #include <grub/safemath.h>
+#include <grub/cpu/efi/memory.h>
 
 GRUB_MOD_LICENSE ("GPLv3+");
 
@@ -111,8 +112,9 @@ grub_cmd_initrd (grub_command_t cmd __attribute__ ((unused)),
 	}
     }
 
-  initrd_mem = grub_efi_allocate_pages_max (0x3fffffff, BYTES_TO_PAGES(size));
-
+  initrd_mem = grub_efi_allocate_pages_max (GRUB_EFI_MAX_ALLOCATION_ADDRESS, BYTES_TO_PAGES(size));
+  if (!initrd_mem)
+    initrd_mem = grub_efi_allocate_pages_max (GRUB_EFI_MAX_USABLE_ADDRESS, BYTES_TO_PAGES(size));
   if (!initrd_mem)
     {
       grub_error (GRUB_ERR_OUT_OF_MEMORY, N_("can't allocate initrd"));
@@ -199,8 +201,11 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
       goto fail;
     }
 
-  params = grub_efi_allocate_pages_max (0x3fffffff,
+  params = grub_efi_allocate_pages_max (GRUB_EFI_MAX_ALLOCATION_ADDRESS,
 					BYTES_TO_PAGES(sizeof(*params)));
+  if (!params)
+    params = grub_efi_allocate_pages_max (GRUB_EFI_MAX_USABLE_ADDRESS,
+					  BYTES_TO_PAGES(sizeof(*params)));
   if (! params)
     {
       grub_error (GRUB_ERR_OUT_OF_MEMORY, "cannot allocate kernel parameters");
@@ -270,8 +275,11 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
 #endif
 
   grub_dprintf ("linuxefi", "setting up cmdline\n");
-  linux_cmdline = grub_efi_allocate_pages_max(0x3fffffff,
+  linux_cmdline = grub_efi_allocate_pages_max(GRUB_EFI_MAX_ALLOCATION_ADDRESS,
 					 BYTES_TO_PAGES(lh->cmdline_size + 1));
+  if (!linux_cmdline)
+    linux_cmdline = grub_efi_allocate_pages_max(GRUB_EFI_MAX_USABLE_ADDRESS,
+						BYTES_TO_PAGES(lh->cmdline_size + 1));
   if (!linux_cmdline)
     {
       grub_error (GRUB_ERR_OUT_OF_MEMORY, N_("can't allocate cmdline"));
@@ -295,16 +303,14 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
 
   start = (lh->setup_sects + 1) * 512;
 
-  kernel_mem = grub_efi_allocate_fixed(lh->pref_address,
-				       BYTES_TO_PAGES(lh->init_size));
-
+  kernel_mem = grub_efi_allocate_pages_max(lh->pref_address,
+					   BYTES_TO_PAGES(lh->init_size));
   if (!kernel_mem)
-    {
-      grub_error_pop();
-      kernel_mem = grub_efi_allocate_pages_max (
-          0x3fffffff, BYTES_TO_PAGES (lh->init_size));
-    }
-
+    kernel_mem = grub_efi_allocate_pages_max(GRUB_EFI_MAX_ALLOCATION_ADDRESS,
+					     BYTES_TO_PAGES(lh->init_size));
+  if (!kernel_mem)
+    kernel_mem = grub_efi_allocate_pages_max(GRUB_EFI_MAX_USABLE_ADDRESS,
+					     BYTES_TO_PAGES(lh->init_size));
   if (!kernel_mem)
     {
       grub_error (GRUB_ERR_OUT_OF_MEMORY, N_("can't allocate kernel"));
