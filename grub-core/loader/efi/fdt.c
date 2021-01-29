812d781f11f0b59b74eac6e5dcb74ad9b7c3438a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-01-29 07:36:42 +0100

    [PATCH] efi: EFI Device Tree Fixup Protocol
    Device-trees are used to convey information about hardware to the operating
    system. Some of the properties are only known at boot time. (One example of
    such a property is the number of the boot hart on RISC-V systems.) Therefore
    the firmware applies fix-ups to the original device-tree. Some nodes and
    properties are added or altered.
    
    When using GRUB's device-tree command the same fix-ups have to be applied.
    The EFI Device Tree Fixup Protocol allows to pass the loaded device tree
    to the firmware for this purpose.
    
    The protocol can
    
    * add nodes and update properties
    * reserve memory according to the /reserved-memory node and the memory
      reservation block
    * install the device-tree as configuration table
    
    With the patch GRUB checks if the protocol is installed and invokes it if
    available. (LP: #1965796)
    
    Link: https://lists.gnu.org/archive/html/grub-devel/2021-02/msg00013.html
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    
    Gbp-Pq: Name efi-EFI-Device-Tree-Fixup-Protocol.patch

--- grub-core/loader/efi/fdt.c
+++ grub-core/loader/efi/fdt.c
@@ -30,12 +30,46 @@
 
 static void *loaded_fdt;
 static void *fdt;
+static grub_efi_guid_t dt_fixup_guid = GRUB_EFI_DT_FIXUP_PROTOCOL_GUID;
 
 #define FDT_ADDR_CELLS_STRING "#address-cells"
 #define FDT_SIZE_CELLS_STRING "#size-cells"
 #define FDT_ADDR_SIZE_EXTRA ((2 * grub_fdt_prop_entry_size (sizeof(grub_uint32_t))) + \
                              sizeof (FDT_ADDR_CELLS_STRING) + \
                              sizeof (FDT_SIZE_CELLS_STRING))
+
+static void *
+grub_fdt_fixup (void)
+{
+  grub_efi_dt_fixup_t *dt_fixup_prot;
+  grub_efi_uintn_t size = 0;
+  grub_efi_status_t status;
+  void *fixup_fdt;
+
+  dt_fixup_prot = grub_efi_locate_protocol (&dt_fixup_guid, 0);
+  if (!dt_fixup_prot)
+    return loaded_fdt;
+
+  grub_dprintf ("linux", "EFI_DT_FIXUP_PROTOCOL available\n");
+
+  status = efi_call_4 (dt_fixup_prot->fixup, dt_fixup_prot, loaded_fdt, &size,
+                       GRUB_EFI_DT_APPLY_FIXUPS | GRUB_EFI_DT_RESERVE_MEMORY);
+  if (status != GRUB_EFI_BUFFER_TOO_SMALL)
+    return loaded_fdt;
+
+  fixup_fdt = grub_realloc (loaded_fdt, size);
+  if (!fixup_fdt)
+    return loaded_fdt;
+  loaded_fdt = fixup_fdt;
+
+  status = efi_call_4 (dt_fixup_prot->fixup, dt_fixup_prot, loaded_fdt, &size,
+                       GRUB_EFI_DT_APPLY_FIXUPS | GRUB_EFI_DT_RESERVE_MEMORY);
+
+  if (status == GRUB_EFI_SUCCESS)
+    grub_dprintf ("linux", "Device tree fixed up via EFI_DT_FIXUP_PROTOCOL\n");
+
+  return loaded_fdt;
+}
 
 void *
 grub_fdt_load (grub_size_t additional_size)
@@ -92,6 +126,9 @@ grub_fdt_install (void)
 
   if (!fdt && !loaded_fdt)
     return GRUB_ERR_NONE;
+
+  if (loaded_fdt)
+    loaded_fdt = grub_fdt_fixup();
 
   b = grub_efi_system_table->boot_services;
   status = b->install_configuration_table (&fdt_guid, fdt ?: loaded_fdt);
